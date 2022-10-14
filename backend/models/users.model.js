e92ea5d2e104ba8fa0637952fec728307887dcf4
JaafarHawli <jaafarhawle@gmail.com>
2022-10-15 02:41:01 +0300

    backend: added users model

---
+++ backend/models/users.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: 'email is required',
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: 'password is required',
+        select: false
+    },
+    type: {
+        type: Number,
+        required: 'type is required',
+    }
+
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
