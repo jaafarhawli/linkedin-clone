203b48d604715fe12992f26472af9233b6d1431d
JaafarHawli <jaafarhawle@gmail.com>
2022-10-15 13:34:22 +0300

    backend: added personSignup function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/users.model');
+const Person = require('../models/people.model');
+
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
@@ -18,6 +20,34 @@ const login = async (req, res)=>{
     res.status(200).json(token)
 }
 
+const personSignup = async (req, res)=>{
+    const {email, password,type,first_name,last_name,country,profile_picture} = req.body;
+    try{
+        const user = new User();
+        user.email = email;
+        user.password = await bcrypt.hash(password, 10);
+        user.type = type;
+
+        await user.save();
+
+        const person = new Person();
+        person.firstname = first_name;
+        person.lastname = last_name;
+        person.country = country;
+        person.profile_url = profile_picture;
+
+        await person.save();
+
+        res.json(user, person);
+
+    }catch(err){
+        res.status(400).json({
+            message: err.message
+        })
+    }
+}
+
 module.exports = {
     login,
+    personSignup
 }
