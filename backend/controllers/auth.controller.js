96b5094a6cf7771949e716ce8e6fe62abf59f96d
JaafarHawli <jaafarhawle@gmail.com>
2022-10-15 13:25:31 +0300

    backend: added login function to auth.controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,23 @@
+const User = require('../models/users.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const login = async (req, res)=>{
+    const {email, password} = req.body;
+    
+    const user = await User.findOne({email}).select("+password");
+
+    if(!user) return res.status(404).json({message: "Invalid Credentials"});
+
+    const isMatch = bcrypt.compare(password, user.password);
+    if(!isMatch) return res.status(404).json({message: "Invalid Credentials"});
+
+    const token = jwt.sign({email: user.email, userType: user.type}, process.env.JWT_SECRET_KEY, {
+        expiresIn: '1h'
+    });
+    res.status(200).json(token)
+}
+
+module.exports = {
+    login,
+}
