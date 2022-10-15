78379bbbdb22c4e169035bae3fc4c7550901ff10
JaafarHawli <jaafarhawle@gmail.com>
2022-10-15 04:53:25 +0300

    backend: modified users model

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -12,11 +12,70 @@ const userSchema = new mongoose.Schema({
         required: 'password is required',
         select: false
     },
-    type: {
-        type: Number,
-        required: 'type is required',
-    }
-
+    firstname: {
+        type: String,
+        required: 'first name is required'
+    },
+    lastname: {
+        type: String,
+        required: 'last name is required'
+    },
+    country: {
+        type: String,
+        required: 'last name is required'
+    },
+    profile_url: {
+        type: String,
+    },
+    banner_url: {
+        type: String,
+    },
+    headline: {
+        type: String,
+    },
+    experience: [{
+        type: {
+            type: String
+        },
+        company: String,
+        startdate: Date,
+        enddate: Date,
+        location: String,
+    }],
+    skills: [{
+        type: String,
+    }],
+    education: [{
+        institution: String,
+        startyear: Number,
+        endyear: Number,
+    }],
+    lisences: [{
+        title: String,
+        institution: String,
+        startyear: Number,
+        endyear: Number,
+    }],
+    projects: [{
+        title: String,
+        institution: String,
+        startyear: Number,
+        endyear: Number,
+        description: String,
+    }],
+    languages: [{
+        language: String,
+        level: String,
+    }],
+    following: [{
+        type: String,
+    }],
+    notifications: [{
+        company_id: Number,
+        title: String, 
+        logo: String,
+        time: Date,
+    }]
 })
 
 const User = mongoose.model('User', userSchema);
