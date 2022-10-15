517f346878d69d8eb86d83a9ea2d8e14c02cb0a4
JaafarHawli <jaafarhawle@gmail.com>
2022-10-15 19:17:06 +0300

    backend: added getPerson function

---
+++ backend/controllers/people.controller.js
@@ -0,0 +1,17 @@
+const User = require('../models/users.model');
+const Person = require('../models/people.model');
+
+const getPerson = async (req, res)=>{
+    const {id} = req.params;
+    const user = await User.findById(id);
+    let data = await Person.findById(id);
+    res.send({user, data});
+}
+
+
+module.exports = {
+    getPerson,
+}
+
+
+// User.findById(id).then((user)=>res.send(user));
--- backend/routes/people.routes.js
+++ backend/routes/people.routes.js
@@ -1,8 +1,9 @@
 const {Router} = require('express');
 const authMiddleware = require('../middlewares/auth.middelware');
 const personMiddleware = require('../middlewares/person.middleware');
+const { getPerson } = require('../controllers/people.controller');
 const router = Router();
 
-
+router.get('/:id', getPerson);
 
 module.exports = router;
