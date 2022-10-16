47a46d56751f6c0bb41f5bbb86cd7d3b0a7634e3
JaafarHawli <jaafarhawle@gmail.com>
2022-10-16 05:24:52 +0300

    backend: added edit company profile function

--- backend/controllers/companies.controller.js
+++ backend/controllers/companies.controller.js
@@ -22,7 +22,68 @@ const addJob = async(req, res) => {
     });
 }
 
+const editProfile = async(req, res) => {
+    const {id, ...data} = req.body
+    const company = await Company.findById(id);
+    const changes = {};
 
+    if(data.name)
+    changes.name = data.name;
+    else
+    changes.name = company.name;
+
+    if(data.url) {
+        const companies = await Company.find();
+        let total_urls = [];
+        companies.forEach(company => {
+        total_urls = total_urls.concat(company.url);        
+        });
+        if (total_urls.includes(url)) 
+        res.status(400).json("url already exists");
+        changes.url = data.url;
+    }
+    else
+    changes.url = company.url;
+    
+    if(data.website)
+    changes.website = data.website;
+    else
+    changes.website = company.website;
+    
+    if(data.industry)
+    changes.industry = data.industry;
+    else
+    changes.industry = company.industry;
+    
+    if(data.size)
+    changes.size = data.size;
+    else
+    changes.size = company.size;
+    
+    if(data.tagline)
+    changes.tagline = data.tagline;
+    else
+    changes.tagline = company.tagline;
+    
+    if(data.overview)
+    changes.overview = data.overview;
+    else
+    changes.overview = company.overview;
+    
+    Company.findByIdAndUpdate(id,{
+        name: changes.name,
+        url: changes.url,
+        website: changes.website,
+        industry: changes.industry, 
+        size: changes.size,
+        tagline: changes.tagline,
+        overview: changes.overview
+    }, async (err) => {
+        if(err)
+        res.status(400).json("invalid input");
+        res.status(200).json("company updated successfully");
+    });
+}
 
 
 module.exports = {
