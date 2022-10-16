21d03d8df46642ef4f395cf1c2f07524df13456b
JaafarHawli <jaafarhawle@gmail.com>
2022-10-17 01:54:06 +0300

    frontend: added handle Submit function

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,22 @@ const app = express();
 app.use(express.json());
 require('dotenv').config();
 require('./config/db.config');
+const headers = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+};
 
 const authRoutes = require('./routes/auth.routes');
-app.use('/auth', authRoutes);
+app.use('/auth', headers, authRoutes);
 
 const peopleRoutes = require('./routes/people.routes');
-app.use('/people', peopleRoutes);
+app.use('/people', headers, peopleRoutes);
 
 const companiesRoutes = require('./routes/companies.routes');
-app.use('/companies', companiesRoutes);
+app.use('/companies', headers, companiesRoutes);
 
 app.listen(process.env.PORT, (err)=>{
     if(err) throw err;
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -4,45 +4,46 @@ import {BsLinkedin} from "react-icons/bs";
 import Button from '../../components/button/Button';
 import banner from '../../images/banner.png';
 import { useState, useEffect } from 'react';
+import axios from '../../api/axios';
+import jwt_decode from "jwt-decode";
 
 const Login = () => {
 
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
 
-//   const handleSubmit = async () => {
-//     const form = {
-//         email: username,
-//         password: password
-//     };
-//     try {
-//         const data = await axios.post('/api/v1/login', form);
-//         const token = data.data.authorisation.token;
-//         if(token) {
-//             localStorage.setItem('token', token);
-//             localStorage.setItem('id', data.data.user._id);
-//             localStorage.setItem('name', data.data.user.name);
-//             localStorage.setItem('email', data.data.user.email);
-//             localStorage.setItem('type', data.data.user.type);
-//             setIsAuthenticated(true);
-//             if(data.data.user.type === 'admin') {
-//                 setUserType('admin');
-//             }
-//             else if(data.data.user.type === 'instructor') {
-//                 setUserType('instructor');
-//             }
-//             else {
-//                 setUserType('student');
-//             }
-//         }
-//         // if(localStorage.type === 'admin') {
-//         //     navigate("/dashboard");
-//         // }
-//         // window.location.reload(false);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+  const handleSubmit = async () => {
+    const form = {
+        email: username,
+        password: password
+    };
+    try {
+        const data = await axios.post('auth/login', form);
+        const token = data.data;
+        const decoded = jwt_decode(token);
+        console.log(decoded);
+        // const token = data.data.authorisation.token;
+        // if(token) {
+        //     localStorage.setItem('token', token);
+        //     localStorage.setItem('id', data.data.user._id);
+        //     localStorage.setItem('name', data.data.user.name);
+        //     localStorage.setItem('email', data.data.user.email);
+        //     localStorage.setItem('type', data.data.user.type);
+        //     setIsAuthenticated(true);
+        //     if(data.data.user.type === 'admin') {
+        //         setUserType('admin');
+        //     }
+        //     else if(data.data.user.type === 'instructor') {
+        //         setUserType('instructor');
+        //     }
+        //     else {
+        //         setUserType('student');
+        //     }
+        // }
+    } catch (error) {
+        console.log(error);
+    }
+  }
 
   return (
     <div>
@@ -63,7 +64,7 @@ const Login = () => {
               <input type="email" className='login-input-email' placeholder='Email' onChange={e => setUserName(e.target.value)} />
               <input type="password" className='login-input-password' placeholder='Password (8+ characters)' onChange={e => setPassword(e.target.value)} />
               <p className='login-agreement'>By clicking Agree & Join, you agree to the LinkedIn <a href="https://www.linkedin.com/legal/user-agreement?trk=homepage-basic_intl-segments-join_join-form-user-agreement">User Agreement</a>, <a href="https://www.linkedin.com/legal/privacy-policy?trk=homepage-basic_intl-segments-join_join-form-privacy-policy">Privacy Policy</a>, and <a href="https://www.linkedin.com/legal/cookie-policy?trk=homepage-basic_intl-segments-join_join-form-cookie-policy">Cookie Policy</a>.</p>
-              <button type="button" className='button login-submit'>Agree & Join</button>
+              <button type="button" className='button login-submit' onClick={handleSubmit}>Agree & Join</button>
             </form>
         </div>
         <div className='banner-container flex'>
