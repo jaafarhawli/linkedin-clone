d48e1fa080658b0b8817b237c45e0c3282ce651c
JaafarHawli <jaafarhawle@gmail.com>
2022-10-16 04:25:14 +0300

    backend: added companies controller

---
+++ backend/controllers/companies.controller.js
@@ -0,0 +1,10 @@
+const User = require('../models/users.model');
+const Person = require('../models/people.model');
+const Job = require('../models/jobs.model');
+const Company = require('../models/companies.model');
+
+
+
+module.exports = {
+    
+}
